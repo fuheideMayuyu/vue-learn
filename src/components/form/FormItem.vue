2d94bd633e06000a3de0652c1d2bce56d0644da9
fuheideMayuyu <xiaguangding@163.com>
2020-03-03 23:09:46 +0800

    .

---
+++ src/components/form/FormItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+
+<script type='text/ecmascript-6'>
+export default {
+  data () {
+    return {}
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted () {},
+
+  methods: {}
+}
+
+</script>
+
+<style lang='' scoped>
+
+</style>
